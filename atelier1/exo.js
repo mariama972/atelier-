ed2d5d8e4756a4502757bdace742185d7d1b53c9
MAME <mbacke.mamediarra@gmmail.com>
2023-10-04 17:00:36 +0200

    atelier1

---
+++ atelier1/exo.js
@@ -0,0 +1,20 @@
+const message = 'Hello world'
+const prenom =' Mame-Diarra'
+// Try edit me
+// Update body text
+document.querySelector('h1').innerHTML = message + prenom
+// Log to console
+console.log(message)
+
+let age = 29
+age= age + 1
+document.querySelector('h3').innerHTML = age
+
+console.log(age)
+
+const number1 = 1
+const number2 = 2
+let result = number1 + number2
+document.querySelector('h4').innerHTML = result
+
+console.log(typeof result)
